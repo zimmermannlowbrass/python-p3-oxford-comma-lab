ef29044765f2ff0540763eff2237a89181e7a7b0
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-05-04 21:24:55 -0500

    completed labwork

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
 def oxford_comma(items):
-    return None
+    l = len(items)
+    if l == 1:
+        return items[0]
+    elif l == 2:
+        return items[0] + ' and ' + items[1]
+    else:
+        phrase = ''
+        for x in range(l - 1):
+            phrase += items[x] + ', '
+        phrase += 'and ' + items[l-1]
+        return phrase
+    
